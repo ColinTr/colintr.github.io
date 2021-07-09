fa8ec6d675841f4e5cda46d3dfe0b0c6bbb2f521
Colin Troisemaine <{ID}+{username}@users.noreply.github.com>
2021-07-09 21:31:33 +0200

    WIP react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Home from './Home';
+import About from './About';
+import NotFound from './NotFound';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>It works !</h1>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <Router basename={process.env.PUBLIC_URL}>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/about" component={About}/>
+                    <Route component={NotFound} status={404}/>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class About extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Home Page</h2>
+                <main>
+                    <p>WIP : Home</p>
+                </main>
+            </div>
+        )
+    }
+}
+
+export default About;
