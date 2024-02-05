24dd966da55377307eff7058d3b60ae76b382e48
ColinTr <56585263+ColinTr@users.noreply.github.com>
2024-02-05 11:28:24 +0100

    Improve the description of work experiences

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -42,7 +42,7 @@ const Experience = () => {
                                 <br/>
                                 <br/>
                                 <ul>
-                                    <li>Realized an comprehensive state-of-the-art of methods for discovering novel classes (Novel Class Discovery, Clustering, Transfer Learning, and Open-World Learning).</li>
+                                    <li>Realized a comprehensive state-of-the-art of methods for discovering novel classes (Novel Class Discovery, Clustering, Transfer Learning, and Open-World Learning).</li>
                                     <li>Developed several novel techniques to discover new classes in an unlabeled set of data, given a labeled set of data, specifically for tabular data.</li>
                                     <li>Collected and curated a dataset of real internet access faults and engineered new features.</li>
                                 </ul>
